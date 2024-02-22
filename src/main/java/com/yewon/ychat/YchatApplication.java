4a2ec9f492b83ae254529d5467e86cd08dbc2d83
jeongye01 <jeongye001@gmail.com>
2024-02-22 17:09:51 +0900

    init

---
+++ src/main/java/com/yewon/ychat/YchatApplication.java
@@ -0,0 +1,13 @@
+package com.yewon.ychat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yewon/ychat/YchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yewon.ychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
